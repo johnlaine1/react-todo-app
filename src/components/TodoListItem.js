92a052cf3edeacc792b13dbcaec4e5962e3e131c
John Laine <john.laine5896@gmail.com>
2017-03-15 21:15:27 +0000

    Fix bug related to marking todos as complete

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -8,14 +8,14 @@ class TodoListItem extends Component {
     const {text, completed} = this.props;
     return (
       <div className="row">
-        <div
-          onClick={this.handleToggleTodo.bind(this)}
+        <div 
           className="col-sm-6 col-sm-offset-3 text-left">
           <li
             className="list-group-item">
             <input 
+              onChange={this.handleToggleTodo.bind(this)}
               type="checkbox" 
-              defaultChecked={completed} /> {text}
+              checked={completed} /> {text}
           </li>
         </div>
       </div>
