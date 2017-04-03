6a63df6bf9d7d0291e531be67f3cfee3de8205e7
John Laine <john.laine5896@gmail.com>
2017-04-03 20:14:51 +0000

    Set up login path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {createStore, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
@@ -7,6 +8,7 @@ import {setTodos, getTodos} from './api/TodoAPI';
 import {startAddTodos} from './actions';
 import rootReducer from './reducers';
 import App from './components/App';
+import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
@@ -19,8 +21,12 @@ store.dispatch(startAddTodos());
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <Route exact path="/" component={Login}/>
+        <Route path="/todos" component={App}/>
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
-
